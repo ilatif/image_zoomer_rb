76d047a4303d8a8c9b5753d47f50533fd5a560b5
ilatif <ilatif.bwp@gmail.com>
2014-09-01 02:24:24 +0500

    Fixed issue of image zooming not working for multiple images

--- app/assets/javascripts/image_zoomer_initializer.js
+++ app/assets/javascripts/image_zoomer_initializer.js
@@ -2,5 +2,7 @@ $(function() {
 	if (typeof __ImageZoomer == "undefined") {
 		__ImageZoomer = {options: {}};
 	}
-	$(".image_zoomer").image_zoomer(__ImageZoomer.options);
+	$(".image_zoomer").each(function() {
+		$(this).image_zoomer(__ImageZoomer.options);
+	});
 });
